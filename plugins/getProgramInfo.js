2510afe6277dae634e24ff7b41143fcfe0a60f6a
6sRyuSK <zikkyou19@gmail.com>
2019-05-14 08:33:01 +0900

    modify baseUrl in getProgramInfo.js

--- plugins/getProgramInfo.js
+++ plugins/getProgramInfo.js
@@ -1,40 +1,40 @@
 import axios from 'axios'
 const getProgramInfo = async (programList) => {
-  const programInfo_baseUrl = 'http://www.onsen.ag/data/api/getMovieInfo/'
-    let programInfoList = []
+  const programInfo_baseUrl = 'https://www.onsen.ag/data/api/getMovieInfo/'
+  let programInfoList = []
 
-    const promises = Object.keys(programList).map((key) => {
-      const item = programList[key];
-      const programInfo_getUrl = encodeURI( programInfo_baseUrl + item)
-      return axios.get(programInfo_getUrl).then(function(response) {
-        // console.log(response)
-        return JSON.parse(response.data.replace('callback(', '').replace(');', ''))
-      
-      })
-    });
-    // promisesを実行
-    await Promise.all(promises).then(function(response) {
-      response.forEach(function(value) {
-        try{
-          if(!value.moviePath.pc){
-            return
-          }
-          programInfoList.push({
-            title: value.title,
-            thumbnailPath: value.thumbnailPath, 
-            moviePath: value.moviePath.pc,
-            update: new Date(value.update),
-            schedule: value.schedule,
-            personality: value.personality,
-            url: value.url,
-          })
-        } catch (err) {
-          // console.log(err)
-        }
-      })
+  const promises = Object.keys(programList).map((key) => {
+    const item = programList[key];
+    const programInfo_getUrl = encodeURI( programInfo_baseUrl + item)
+    return axios.get(programInfo_getUrl).then(function(response) {
+      // console.log(response)
+      return JSON.parse(response.data.replace('callback(', '').replace(');', ''))
+    
     })
+  });
+  // promisesを実行
+  await Promise.all(promises).then(function(response) {
+    response.forEach(function(value) {
+      try{
+        if(!value.moviePath.pc){
+          return
+        }
+        programInfoList.push({
+          title: value.title,
+          thumbnailPath: value.thumbnailPath, 
+          moviePath: value.moviePath.pc,
+          update: new Date(value.update),
+          schedule: value.schedule,
+          personality: value.personality,
+          url: value.url,
+        })
+      } catch (err) {
+        // console.log(err)
+      }
+    })
+  })
 
-    return programInfoList
+  return programInfoList
 }
 
 export default getProgramInfo
