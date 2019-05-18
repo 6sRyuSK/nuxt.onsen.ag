be127ef2066b200aff70f3754a06d4ff02613a83
6sRyuSK <zikkyou19@gmail.com>
2019-05-19 07:44:44 +0900

    Remove some dead code

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <section class="container">
-    <!-- <v-btn @click="getUserWatching">aaa</v-btn> -->
-    <!-- <h3>{{userWatching}}</h3> -->
     <inputSearch />
     <programList />
   </section>
@@ -12,34 +10,21 @@ import axios from 'axios'
 import programList from '~/components/programList'
 import inputSearch from '~/components/inputSearch'
 import getProgramInfo from '~/plugins/getProgramInfo'
-import { request, GraphQLClient } from 'graphql-request'
 export default {
   components: {
     programList,
     inputSearch,
   },
-  data() {
-    return {
-      client: 0,
-      endPoint: 'https://api.annict.com/graphql',
-      userWatching: []
-    }
-  },
   created() {
-    this.clientInitialize()
+    // this.getPreProgram()
   },
   methods: {
-    clientInitialize() {
-      this.client = new GraphQLClient(this.endPoint, { headers: {Authorization: "Bearer 665698b3e3df57bb247c422dfe42b78cf40585a70afb3781d17ccc8699584df5"}})
-    },
-    getUserWatching() {
-      const query = `
-        query { user( username: "6sRyuSK" ) { name works { edges{node {
-        twitterUsername
-      }} }} }
-      `
-      this.client.request(query).then(data => this.userWatching = data)
-    }
+    // async getPreProgram(){
+      
+    // }
+  },
+  mounted() {
+    
   },
   async fetch ({ store, params }) {
     const programList_baseUrl = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
