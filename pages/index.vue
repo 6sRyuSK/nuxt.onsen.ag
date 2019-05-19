6c49f06b49557af6247191ff1fac0fe53aef3de3
6sRyuSK <zikkyou19@gmail.com>
2019-05-19 09:26:14 +0900

    add getPreProgram, Not working

--- pages/index.vue
+++ pages/index.vue
@@ -9,32 +9,39 @@
 import axios from 'axios'
 import programList from '~/components/programList'
 import inputSearch from '~/components/inputSearch'
-import getProgramInfo from '~/plugins/getProgramInfo'
+// import getProgramInfo from '~/plugins/getProgramInfo'
+import getJsonp from '~/plugins/getJsonp'
 export default {
   components: {
     programList,
     inputSearch,
   },
   created() {
-    // this.getPreProgram()
+    this.getPreProgram()
   },
   methods: {
-    // async getPreProgram(){
+    async getPreProgram(){
+      // const programList_baseUrl = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
+      // const programList_getUrl = encodeURI(programList_baseUrl)
+      // const res = await axios.get(programList_getUrl).then(function(response) {
+      //   return response.data.result
+      // })
+      const headNode = document.querySelector('body')
+      const paddingScript = document.createElement('script')
+      paddingScript.src = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
+      // paddingScript.type = 'application/json'
+      // paddingScript.id = 'shownMovie'
+      headNode.appendChild(paddingScript)
+      const elem = document.getElementById("shownMovie");
+      // const json = elem.textContent
+      console.log(elem, "--------------------------------")
       
-    // }
-  },
-  mounted() {
-    
+      // const programInfoList = await getProgramInfo(res)
+      // thid.$store.commit('setprogramsInfoList', programInfoList)
+    }
   },
   async fetch ({ store, params }) {
-    const programList_baseUrl = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
-    const programList_getUrl = encodeURI(programList_baseUrl)
-    const res = await axios.get(programList_getUrl).then(function(response) {
-      return response.data.result
-    })
     
-    const programInfoList = await getProgramInfo(res)
-    store.commit('setprogramsInfoList', programInfoList)
 
   }
   
