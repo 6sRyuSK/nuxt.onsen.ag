a058e487e650bb25cd1a74686266fdfa2c1e3df6
6sRyuSK <zikkyou19@gmail.com>
2019-05-25 12:06:45 +0900

    use from fetch to asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -9,40 +9,22 @@
 import axios from 'axios'
 import programList from '~/components/programList'
 import inputSearch from '~/components/inputSearch'
-// import getProgramInfo from '~/plugins/getProgramInfo'
-import getJsonp from '~/plugins/getJsonp'
+import getProgramInfo from '~/plugins/getProgramInfo'
+// import getJsonp from '~/plugins/getJsonp'
 export default {
   components: {
     programList,
     inputSearch,
   },
-  created() {
-    this.getPreProgram()
-  },
-  methods: {
-    async getPreProgram(){
-      // const programList_baseUrl = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
-      // const programList_getUrl = encodeURI(programList_baseUrl)
-      // const res = await axios.get(programList_getUrl).then(function(response) {
-      //   return response.data.result
-      // })
-      const headNode = document.querySelector('body')
-      const paddingScript = document.createElement('script')
-      paddingScript.src = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
-      // paddingScript.type = 'application/json'
-      // paddingScript.id = 'shownMovie'
-      headNode.appendChild(paddingScript)
-      const elem = document.getElementById("shownMovie");
-      // const json = elem.textContent
-      console.log(elem, "--------------------------------")
-      
-      // const programInfoList = await getProgramInfo(res)
-      // thid.$store.commit('setprogramsInfoList', programInfoList)
-    }
-  },
-  async fetch ({ store, params }) {
+  async asyncData({ store }) {
+    const programList_baseUrl = 'https://www.onsen.ag/api/shownMovie/shownMovie.json'
+    const programList_getUrl = encodeURI(programList_baseUrl)
+    const res = await axios.get(programList_getUrl).then(function(response) {
+      return response.data.result
+    })
     
-
+    const programInfoList = await getProgramInfo(res)
+    store.commit('setprogramsInfoList', programInfoList)
   }
   
   
